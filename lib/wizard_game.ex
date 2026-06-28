e20ad4e6fe1e8fafb3c5313af5aa7b2add889546
Cameron <cmrnwlsh@gmail.com>
2026-06-27 22:54:37 -0400

    remove the docs and tests cuz i dont need them

--- lib/wizard_game.ex
+++ lib/wizard_game.ex
@@ -1,17 +1,4 @@
 defmodule WizardGame do
-  @moduledoc """
-  Documentation for `WizardGame`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> WizardGame.hello()
-      :world
-
-  """
   def hello do
     :world
   end
--- test/wizard_game_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule WizardGameTest do
-  use ExUnit.Case
-  doctest WizardGame
-
-  test "greets the world" do
-    assert WizardGame.hello() == :world
-  end
-end
