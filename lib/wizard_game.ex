932e1a153707ab78593ebb05433a24d336e25016
Cameron <cmrnwlsh@gmail.com>
2026-06-27 22:35:33 -0400

    my first jj commit

---
+++ lib/wizard_game.ex
@@ -0,0 +1,18 @@
+defmodule WizardGame do
+  @moduledoc """
+  Documentation for `WizardGame`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> WizardGame.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
